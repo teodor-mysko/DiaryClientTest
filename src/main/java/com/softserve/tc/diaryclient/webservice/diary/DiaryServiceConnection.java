430d1a6bfed22a3cb8305310fdb421f64295241c
AndriyZhmurkevych <andriyzhmurkevych@gmail.com>
2015-09-24 00:47:37 +0300

    Added stub for Xstream parser

--- src/main/java/com/softserve/tc/diaryclient/webservice/diary/DiaryServiceConnection.java
+++ src/main/java/com/softserve/tc/diaryclient/webservice/diary/DiaryServiceConnection.java
@@ -9,22 +9,23 @@ import javax.xml.ws.Service;
 import com.softserve.tc.diary.webservice.DiaryService;
 
 public class DiaryServiceConnection {
-
-	static DiaryService diaryService;
-
-	public static DiaryService getDairyServicePort(){
-		QName qname = new QName("http://webservice.diary.tc.softserve.com/", "DiaryServiceImplService");
-		URL wsdlUrl;
-		try {
-			System.out.println("Say something...");
-			wsdlUrl = new URL("http://localhost:8080/Diary/DiaryService?wsdl");
-			Service service = Service.create(wsdlUrl, qname);
-			DiaryService port = service.getPort(DiaryService.class);
-			System.out.println(port.sayHello("Java-159.1"));
-			return port;
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		return diaryService;
-	}
+    
+    static DiaryService diaryService;
+    
+    public static DiaryService getDairyServicePort() {
+        QName qname = new QName("http://webservice.diary.tc.softserve.com/",
+                "DiaryServiceImplService");
+        URL wsdlUrl;
+        try {
+            System.out.println("Say something...");
+            wsdlUrl = new URL("http://localhost:8080/Diary/DiaryService?wsdl");
+            Service service = Service.create(wsdlUrl, qname);
+            DiaryService port = service.getPort(DiaryService.class);
+            System.out.println(port.sayHello("Java-159.1"));
+            return port;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return diaryService;
+    }
 }
--- src/test/java/com/softserve/tc/diaryclient/autosave/RecordJAXBParserTest.java
+++ src/test/java/com/softserve/tc/diaryclient/autosave/RecordJAXBParserTest.java
@@ -18,57 +18,60 @@ import com.softserve.tc.diaryclient.entity.Record;
 /**
  * 
  * @author Mykola-
- *
+ *         
  */
 public class RecordJAXBParserTest {
-	String line;
-	StringBuilder sb = new StringBuilder();
-	BufferedReader br;
-
-	@Test
-	public void testMarshalText() {
-		RecordJAXBParser parser = new RecordJAXBParser();
-		Record record = new Record();
-		record.setUser_name("Thomas");
-		record.setCreated_time("2015-05-20 12:00:56");
-		record.setText("#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!");
-		record.setSupplement("http:/bigBoss/works/perfectly");
-		// UserSession user = new UserSession();
-		// user.setNickName("Niikkkooo");
-		// System.out.println(user);
-
-		parser.marshalText(record);
-
-		/**
-		 * read from xml file to get its contents and than take it to assert
-		 */
-		try {
-			br = new BufferedReader(new FileReader(new File("D:\\DiaryClient\\Chicago.xml")));
-
-			while ((line = br.readLine()) != null) {
-				sb.append(line.trim());
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		String output = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><RECORD><CREATED_TIME>2015-05-20 12:00:56</CREATED_TIME><SUPPLEMENT_URL>http:/bigBoss/works/perfectly</SUPPLEMENT_URL><TEXT>#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!</TEXT><USER_NAME>Thomas</USER_NAME></RECORD>";
-		assertEquals(output, sb.toString());
-		assertTrue(sb.toString().contains("<USER_NAME>Thomas</USER_NAME>"));
-		assertNotNull(sb);
-	}
-
-	@Test
-	public void unMarshalText() {
-		RecordJAXBParser parser = new RecordJAXBParser();
-		Record record = new Record();
-		String unmarshalOutput = parser.unMarshalText(record);
-
-		String expectedOutput = "Record [id_rec=null, user_name=Thomas, created_time=2015-05-20 12:00:56, text=#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!, supplement=http:/bigBoss/works/perfectly]";
-		assertEquals(expectedOutput, unmarshalOutput);
-		assertTrue(unmarshalOutput.toString().contains("user_name=Thomas"));
-		assertNotNull(unmarshalOutput);
-	}
+    String line;
+    StringBuilder sb = new StringBuilder();
+    BufferedReader br;
+    
+    @Test
+    public void testMarshalText() {
+        RecordJAXBParser parser = new RecordJAXBParser();
+        Record record = new Record();
+        record.setUser_name("Thomas");
+        record.setCreated_time("2015-05-20 12:00:56");
+        record.setText(
+                "#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!");
+        record.setSupplement("http:/bigBoss/works/perfectly");
+        // UserSession user = new UserSession();
+        // user.setNickName("Niikkkooo");
+        // System.out.println(user);
+        
+        parser.marshalText(record);
+        
+        /**
+         * read from xml file to get its contents and than take it to assert
+         */
+        try {
+            br = new BufferedReader(
+                    new FileReader(new File("D:\\DiaryClient\\Chicago.xml")));
+                    
+            while ((line = br.readLine()) != null) {
+                sb.append(line.trim());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        String output =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><RECORD><CREATED_TIME>2015-05-20 12:00:56</CREATED_TIME><SUPPLEMENT_URL>http:/bigBoss/works/perfectly</SUPPLEMENT_URL><TEXT>#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!</TEXT><USER_NAME>Thomas</USER_NAME></RECORD>";
+        assertEquals(output, sb.toString());
+        assertTrue(sb.toString().contains("<USER_NAME>Thomas</USER_NAME>"));
+        assertNotNull(sb);
+    }
+    
+    @Test
+    public void unMarshalText() {
+        RecordJAXBParser parser = new RecordJAXBParser();
+        Record unmarshaledRecord = parser.unMarshalText(null);
+        
+        String expectedOutput =
+                "Record [id_rec=null, user_name=Thomas, created_time=2015-05-20 12:00:56, text=#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!, supplement=http:/bigBoss/works/perfectly]";
+        assertEquals(expectedOutput, unmarshaledRecord.toString());
+        assertTrue(unmarshaledRecord.toString().contains("user_name=Thomas"));
+        assertNotNull(unmarshaledRecord);
+    }
 }
