eb4ba5fc0882e91cccf0d8331e29e410d85c9beb
AndriyZhmurkevych <andriyzhmurkevych@gmail.com>
2015-09-24 22:54:14 +0300

    Added XStreamRecordParser Implementation

--- src/main/java/com/softserve/tc/diaryclient/autosave/XSteamRecordParser.java
+++ src/main/java/com/softserve/tc/diaryclient/autosave/XSteamRecordParser.java
@@ -4,19 +4,23 @@ import org.apache.log4j.Logger;
 
 import com.softserve.tc.diaryclient.entity.Record;
 import com.softserve.tc.diaryclient.log.Log;
+import com.thoughtworks.xstream.XStream;
 
 public class XSteamRecordParser implements XMLParser {
     
     private static Logger LOG =
             Log.init(XSteamRecordParser.class.toString());
-            
+    XStream xs = new XStream();
+    
     public String marshalText(Record record) {
-        
-        return null;
+        LOG.debug(String.format("Converting to XML %s", record.toString()));
+        xs.alias("record", Record.class);
+        return xs.toXML(record);
     }
     
     public Record unMarshalText(String record) {
-        
-        return null;
+        LOG.debug(String.format("Converting XML to record %s", record));
+        xs.alias("record", Record.class);
+        return (Record) xs.fromXML(record);
     }
 }
---
+++ src/test/java/com/softserve/tc/diaryclient/autosave/XMLSteamRecordParserTest.java
@@ -0,0 +1,31 @@
+package com.softserve.tc.diaryclient.autosave;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+import com.softserve.tc.diaryclient.entity.Record;
+
+public class XMLSteamRecordParserTest {
+    
+    @Test
+    public void testMarshalText() {
+        
+        XMLParser parser = new XSteamRecordParser();
+        
+        Record record = new Record();
+        record.setUser_name("Thomas");
+        record.setCreated_time("2015-05-20 12:00:56");
+        record.setText(
+                "#Hello, I'm Thomas #Shelby. I'm from #Birmingham, #England!");
+        record.setSupplement("http:/bigBoss/works/perfectly");
+        
+        String strResult = parser.marshalText(record);
+        
+        assertNotNull(strResult);
+        
+        Record record1 = parser.unMarshalText(strResult);
+        
+        assertEquals(record.toString(), record1.toString());
+    }
+}
