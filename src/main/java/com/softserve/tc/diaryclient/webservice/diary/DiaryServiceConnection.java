96566fdf51edc3cfe17a37b1b21da72a98b5f70f
Maryana Bytsiv <Maryana Bytsiv>
2015-09-24 11:30:26 +0300

    magic variables

--- src/main/java/com/softserve/tc/diaryclient/webservice/diary/DiaryServiceConnection.java
+++ src/main/java/com/softserve/tc/diaryclient/webservice/diary/DiaryServiceConnection.java
@@ -11,13 +11,19 @@ import com.softserve.tc.diary.webservice.DiaryService;
 public class DiaryServiceConnection {
 
 	static DiaryService diaryService;
+	public static final String NAME_SPACE_URI = "http://webservice.diary.tc.softserve.com/";
+	public static final String LOCAL_PART = "DiaryServiceImplService";
+	public static final String WSDL = "http://localhost:8080/Diary/DiaryService?wsdl";
+	
+	private DiaryServiceConnection() {
+	}
 
 	public static DiaryService getDairyServicePort(){
-		QName qname = new QName("http://webservice.diary.tc.softserve.com/", "DiaryServiceImplService");
-		URL wsdlUrl;
+		
+		QName qname = new QName(NAME_SPACE_URI, LOCAL_PART);
+		
 		try {
-			System.out.println("Say something...");
-			wsdlUrl = new URL("http://localhost:8080/Diary/DiaryService?wsdl");
+			URL wsdlUrl = new URL(WSDL);
 			Service service = Service.create(wsdlUrl, qname);
 			DiaryService port = service.getPort(DiaryService.class);
 			System.out.println(port.sayHello("Java-159.1"));
