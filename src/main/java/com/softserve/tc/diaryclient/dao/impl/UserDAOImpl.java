6451f5aab8d57e60434ba2ac03903a516ff789ea
Bug5Bunny <mural@ex.ua>
2015-09-29 16:17:42 +0300

    Fixed spring security

---
+++ src/main/java/com/softserve/tc/diaryclient/dao/impl/UserDAOImpl.java
@@ -0,0 +1,14 @@
+package com.softserve.tc.diaryclient.dao.impl;
+
+import com.softserve.tc.diary.webservice.DiaryService;
+import com.softserve.tc.diaryclient.entity.User;
+import com.softserve.tc.diaryclient.webservice.diary.DiaryServiceConnection;
+
+public class UserDAOImpl {
+
+    public static User findByUserName(String username) {
+        DiaryService port = DiaryServiceConnection.getDairyServicePort();
+        com.softserve.tc.diary.entity.User user = port.getUserByNickName(username);
+        return new User(user.getNick_name(),user.getPassword(),"ROLE_USER");
+    }
+}
--- src/main/java/com/softserve/tc/diaryclient/service/CustomUserDetailsService.java
+++ src/main/java/com/softserve/tc/diaryclient/service/CustomUserDetailsService.java
@@ -11,25 +11,46 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service("CustomUserDetailsService") 
+import com.softserve.tc.diaryclient.dao.impl.UserDAOImpl;
+
+@Service("CustomUserDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
-    
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
-            
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        com.softserve.tc.diaryclient.entity.User user = UserDAOImpl.findByUserName(username);
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-        if (username.equals("admin")) {
-
+        if (user.getUsername().equals("admin")) {
             authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-            return new User("admin", "21232f297a57a5a743894a0e4a801fc3", authorities);
-
-        } else if (username.equals("user")) {
-
-            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-            return new User("user", "ee11cbb19052e40b07aac0ca060c23ee", authorities);
-        } else {
-            throw new UsernameNotFoundException("User not found: " + username);
         }
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new User(user.getUsername(), user.getPassword(), true, true, true, true, authorities);
     }
+
+    // buildUserForAuthentication(com.softserve.tc.diaryclient.entity.User user,
+    // List<GrantedAuthority> authorities) {
+    // return new User(user.getUsername(), user.getPassword(), true, true, true,
+    // true, authorities);
+    // }
+    //
+    // private List<GrantedAuthority> buildUserAuthority() {
+    //
+    // Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
+    //
+    // setAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+    // setAuths.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    //
+    // List<GrantedAuthority> Result = new
+    // ArrayList<GrantedAuthority>(setAuths);
+    //
+    // return Result;
+    // }
+    //
+    // public UserDAO getUserDAO() {
+    // return userDAO;
+    // }
+    //
+    // public void setUserDAO(UserDAO userDAO) {
+    // this.userDAO = userDAO;
+    // }
 }
