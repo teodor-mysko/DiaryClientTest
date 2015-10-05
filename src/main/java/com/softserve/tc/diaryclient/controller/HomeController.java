3efa22cb653bf5d57bf126dae2e1d738e67fb21c
Maryana Bytsiv <Maryana Bytsiv>
2015-10-05 12:25:47 +0300

    home page

--- src/main/java/com/softserve/tc/diaryclient/controller/HomeController.java
+++ src/main/java/com/softserve/tc/diaryclient/controller/HomeController.java
@@ -1,35 +1,45 @@
 package com.softserve.tc.diaryclient.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Handles requests for the application home page.
- */
+import com.google.gson.Gson;
+import com.softserve.tc.diary.entity.Record;
+import com.softserve.tc.diary.webservice.DiaryService;
+import com.softserve.tc.diaryclient.webservice.diary.DiaryServiceConnection;
+
 @Controller
 public class HomeController {
 
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate );
-
 		return "home";
 	}
 
+	@RequestMapping(value = "/getRecordsByDay", method = RequestMethod.GET)
+	public @ResponseBody String getRecordsByDay(@RequestParam("selected") String date, HttpServletRequest request) {
+
+		if (request.getUserPrincipal() == null) {
+			return new Gson().toJson("User is not logged!");
+		}
+
+		String userNickName = request.getUserPrincipal().getName();
+		DiaryService port = DiaryServiceConnection.getDairyServicePort();
+		List<Record> recordsList = port.getAllRecordsByDate(userNickName, date + " 00:00:00");
+
+		if (recordsList.size() == 0) {
+			return new Gson().toJson("< no notes >");
+		}
+
+		return new Gson().toJson(recordsList.toString());
+	}
 }
